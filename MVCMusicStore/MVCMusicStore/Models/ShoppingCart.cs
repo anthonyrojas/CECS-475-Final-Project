3ffd4169ddbb2874b65ff684e0c16e72227272b5
anthonyrojas <anthonyrojas095@gmail.com>
2017-12-14 17:08:57 -0800

    Page 93

---
+++ MVCMusicStore/MVCMusicStore/Models/ShoppingCart.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStore.Models
+{
+    public partial class ShoppingCart
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        string ShoppingCartId { get; set; }
+
+        public static ShoppingCart GetCart(HttpContextBase context)
+        {
+            var cart = new ShoppingCart();
+            cart.ShoppingCartId = cart.GetCartId(context);
+            return cart;
+        }
+
+        //helper method to simplify shopping cart calls
+        public static ShoppingCart GetCart(Controller controller)
+        {
+            return GetCart(controller.HttpContext);
+        }
+
+        public void AddToCart(Album album)
+        {
+            //Get the matching cart cand album instances
+            var cartItem = storeDB.Carts.SingleOrDefault(
+                c => c.CartId == ShoppingCartId 
+                && c.AlbumId == album.AlbumId);
+        }
+
+        public void AddToCart(Album album)
+        {
+            // Get the matching cart and album instances
+            var cartItem = storeDB.Carts.SingleOrDefault(
+           c => c.CartId == ShoppingCartId
+           && c.AlbumId == album.AlbumId);
+            if (cartItem == null)
+            {
+                // Create a new cart item if no cart item exists
+                cartItem = new Cart
+                {
+                    AlbumId = album.AlbumId,
+                    CartId = ShoppingCartId,
+                    Count = 1,
+                    DateCreated = DateTime.Now
+                };
+                storeDB.Carts.Add(cartItem);
+            }
+            else
+            {
+                // If the item does exist in the cart, then add one to the quantity
+                cartItem.Count++;
+            }
+            // Save changes
+            storeDB.SaveChanges();
+        }
+    }
+}
